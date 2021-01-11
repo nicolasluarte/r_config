92f1df0045cc4d273a1adb7c90401db351a366e6
nicolasluarte <l.luarte@uc.cl>
2021-01-11 01:27:29 -0300

    update

--- installPackages.R
+++ installPackages.R
@@ -1,6 +1,3 @@
-r = getOption("repos")
-r["CRAN"] = "https://cloud.r-project.org/"
-options(repos = r)
 list <- read.csv('packageList.csv', header=FALSE)
 for (lib in list){
 	install.packages(lib, repos = 'https://cran.rstudio.com')
